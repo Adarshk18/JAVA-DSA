e22aca2c800789e9f3867ce80111b8bb92c15599
FOODOHOLIC <adarshkohli183@gmail.com>
2022-06-30 11:23:59 +0530

    DSA

--- com/adarsh/RichestCustomerWealth.java
+++ com/adarsh/RichestCustomerWealth.java
@@ -1,4 +1,21 @@
 package com.adarsh;
 
 public class RichestCustomerWealth {
+    public static void main(String[] args) {
+
+    }
+    public int maximumWealth(int[][] accounts ){
+        int ans = Integer.MIN_VALUE;
+        for (int[] ints : accounts) {
+            int sum = 0;
+            for (int anInt : ints) {
+                sum += anInt;
+            }
+            if (sum > ans) {
+                ans = sum;
+            }
+        }
+        return ans;
+    }
 }
+
